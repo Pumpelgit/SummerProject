3cea6251db95d9b152ce86432b941cdeb27e43f0
Pumpel <romanbaos@gmail.com>
2020-08-23 21:17:08 +0200

    found out why likes were not working. Had to add route after env.HOST

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 function like(e) {
 	const button = e.currentTarget;
-	axios.post(process.env.HOST || `http://localhost:3000/projects/${button.id}/like`)
+	axios.post(`${process.env.HOST}/projects/${button.id}/like` || `http://localhost:3000/projects/${button.id}/like`)
 		.then(res => {
 			const add = res.data.like;
 			button.querySelector('.likes-count').innerText = Number(button.querySelector('.likes-count').innerText) + add;
